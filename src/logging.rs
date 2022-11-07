7424fca4354e8399cf6df899c9aa45e1a6329354
Joshua Potts <8704475+iamjpotts@users.noreply.github.com>
2022-11-06 19:57:06 -0600

    Custom log level

--- src/logging.rs
+++ src/logging.rs
@@ -3,8 +3,12 @@ use simple_logger::SimpleLogger;
 
 /// Configure some defaults that are sensible for examples and tests.
 pub fn enable() {
+    enable_with_level(LevelFilter::Info)
+}
+
+pub fn enable_with_level(level: LevelFilter) {
     SimpleLogger::new()
-        .with_level(LevelFilter::Info)
+        .with_level(level)
         .with_threads(true)
         .env()
         .init()
