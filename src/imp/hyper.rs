85324632e4e23d5d44c866cd59852d0a47a6ff1c
Joshua Potts <8704475+iamjpotts@users.noreply.github.com>
2024-01-03 21:56:11 -0600

    Upgrade hyper to 1.0

--- src/imp/hyper.rs
+++ src/imp/hyper.rs
@@ -1,24 +1,32 @@
+use http::Response;
+use http_body_util::{BodyExt, Empty};
+use hyper::body::{Body, Bytes, Incoming};
+use hyper_util::client::legacy::Client;
+use hyper_util::client::legacy::connect::HttpConnector;
+use hyper_util::rt::TokioExecutor;
 use native_tls::TlsConnector;
 use crate::http_status_tests::StatusTest;
 use crate::http_errors::HttpError;
 
 pub(crate) struct HyperHttp {}
 
+const MAX_IDLE_PER_HOST: usize = 1;
+
 impl HyperHttp {
 
-    fn build_client_http() -> hyper::Client<hyper::client::HttpConnector> {
-        hyper::Client::builder()
-            .pool_max_idle_per_host(0)
-            .build::<_, hyper::Body>(hyper::client::HttpConnector::new())
+    fn build_client_http<D: Send, B: Body<Data = D> + Send>() -> Client<HttpConnector, B> {
+        Client::builder(TokioExecutor::new())
+            .pool_max_idle_per_host(MAX_IDLE_PER_HOST)
+            .build(HttpConnector::new())
     }
 
-    fn build_client_from_tls(tls: TlsConnector) -> hyper::Client<hyper_tls::HttpsConnector<hyper::client::HttpConnector>> {
-        let mut inner = hyper::client::HttpConnector::new();
+    fn build_client_from_tls<D: Send, B: Body<Data = D> + Send>(tls: TlsConnector) -> Client<hyper_tls::HttpsConnector<HttpConnector>, B> {
+        let mut inner = HttpConnector::new();
         inner.enforce_http(false);
 
-        hyper::Client::builder()
-            .pool_max_idle_per_host(0)
-            .build::<_, hyper::Body>(hyper_tls::HttpsConnector::from((inner, tls.into())))
+        Client::builder(TokioExecutor::new())
+            .pool_max_idle_per_host(MAX_IDLE_PER_HOST)
+            .build(hyper_tls::HttpsConnector::from((inner, tls.into())))
     }
 
     pub(crate) async fn get_text_http<T>(&self, url: &str, status_test: &T) -> Result<String, HttpError>
@@ -27,8 +35,8 @@ impl HyperHttp {
     {
         let client = Self::build_client_http();
 
-        let request: hyper::Request<hyper::Body> = hyper::http::Request::get(url)
-            .body(hyper::Body::empty())?;
+        let request = hyper::http::Request::get(url)
+            .body(empty_body())?;
 
         let response = client
             .request(request)
@@ -43,8 +51,8 @@ impl HyperHttp {
     {
         let client = Self::build_client_from_tls(tls);
 
-        let request: hyper::Request<hyper::Body> = hyper::http::Request::get(url)
-            .body(hyper::Body::empty())?;
+        let request = hyper::http::Request::get(url)
+            .body(empty_body())?;
 
         let response = client
             .request(request)
@@ -53,20 +61,37 @@ impl HyperHttp {
         Self::parse_response(response, status_test).await
     }
 
-    async fn parse_response<T>(response: hyper::Response<hyper::Body>, status_test: &T) -> Result<String, HttpError>
+    async fn parse_response<T>(response: Response<Incoming>, status_test: &T) -> Result<String, HttpError>
     where
         T: StatusTest
     {
         if status_test.test(response.status()) {
-            let response_body = hyper::body::to_bytes(response)
-                .await?;
+            let response_body = incoming_bytes(response).await?;
 
-            Ok(String::from_utf8(response_body.into())?)
+            Ok(String::from_utf8(response_body)?)
         }
         else {
             Err(HttpError::Status(response.status()))
         }
     }
+}
+
+fn empty_body() -> Empty<Bytes> {
+    Empty::new()
+}
+
+async fn incoming_bytes(mut response: Response<Incoming>) -> Result<Vec<u8>, hyper::Error> {
+    let mut response_body: Vec<u8> = Vec::new();
+
+    while let Some(frame_result) = response.frame().await {
+        let frame = frame_result?;
+
+        if let Some(segment) = frame.data_ref() {
+            response_body.extend_from_slice(segment.iter().as_slice());
+        }
+    }
+
+    Ok(response_body)
 }
 
 #[cfg(test)]
